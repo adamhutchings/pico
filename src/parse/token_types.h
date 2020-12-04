cc5f0ef559ee6f8ca87542cc586e7809d99ac424
Adam Hutchings <55220104+adamhutchings@users.noreply.github.com>
2020-12-03 22:59:40 -0800

    Add token types

---
+++ src/parse/token_types.h
@@ -0,0 +1,42 @@
+/**
+ * All possible token types. Used by both the tokenizer and the parser.
+ *
+ * Author: Adam Hutchings (adam.abahot@gmail.com)
+ */
+ 
+ #ifndef PICO_PARSE_TOKEN_TYPES_H
+ #define PICO_PARSE_TOKEN_TYPES_H
+ 
+ enum {
+ 
+  IDENTIFIER,
+  LITERAL,
+  SEMICOLON,
+  COLON,
+  DOT,
+  COMMA,
+  OPENPAREN,
+  CLOSEPAREN,
+  OPENBRACKET,
+  CLOSEBRACKET,
+  OPENBRACE,
+  CLOSEBRACE,
+  ASSIGN_OP,
+  NORMAL_OP,
+  CREMENT_OP,
+  UNARY_OP,
+  TYPENAME,
+  CLASS,
+  ACCESS_MOD,
+  JUMP_STATEMENT,
+  IF,
+  ELIF,
+  ELSE,
+  WHILE,
+  LOOP,
+  USE
+ 
+ };
+ 
+ #endif /* PICO_PARSE_TOKEN_TYPES_H */
+ 
